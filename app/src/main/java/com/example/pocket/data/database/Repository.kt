369285cb6f6a726ad214cb86e0f8ce1cf4244f83
Marsh Royden <marshroyden@gmail.com>
2021-04-25 00:21:54 +0530

    1)Added new checkIfUrlExists() method to Dao.kt 2)Changed the Repository.saveUrl() method to save the url   only when the url does not already exist in the database.

--- app/src/main/java/com/example/pocket/data/database/Repository.kt
+++ app/src/main/java/com/example/pocket/data/database/Repository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.util.Log
 import com.example.pocket.data.network.PocketNetwork
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,8 +14,6 @@ import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
 
-private const val TAG = "Repository"
-
 class Repository private constructor(context: Context) {
     private val mDatabase = UrlDatabase.getInstance(context)
     private val mDao = mDatabase.getDao()
@@ -23,12 +22,35 @@ class Repository private constructor(context: Context) {
     private val mCoroutineScope = CoroutineScope(Dispatchers.IO)
     val getUrls = mDao.getAllUrls()
 
+    /**
+     * Used for saving the url,absolute path of the thumbnail and the
+     * thumbnail itself to the internal storage.It will save the url
+     * only if the url doesn't already exist in the database.Url,absolute
+     * path will be stored in the database.Whereas,the thumbnail image will be
+     * stored to the internal storage of the device.
+     * @param urlString string representing the complete url
+     * @param thumbnail the drawable image that will be used for the thumbnail
+     */
     suspend fun saveUrl(urlString: String, thumbnail: Drawable?) {
-        val url = URL(urlString)
-        val urlContentTitle = mNetwork.fetchWebsiteContentTitle(urlString)
-        val imageAbsolutePath = thumbnail?.let { saveImageToInternalStorage(thumbnail, url.host + urlContentTitle) }
-        mDao.insertUrl(UrlEntity(urlString, urlContentTitle, imageAbsolutePath))
+        if (!urlExists(urlString)) {
+            val url = URL(urlString)
+            val urlContentTitle = mNetwork.fetchWebsiteContentTitle(urlString)
+            val imageAbsolutePath =
+                thumbnail?.let { saveImageToInternalStorage(thumbnail, url.host + urlContentTitle) }
+            mDao.insertUrl(UrlEntity(urlString, urlContentTitle, imageAbsolutePath))
+        }
     }
+
+    /**
+     * Check whether the url already exists in the database or not.
+     * @param urlString string representing the complete url
+     * @return true if exists else false
+     */
+    private suspend fun urlExists(urlString: String) =
+        when (withContext(Dispatchers.IO) { mDao.checkIfUrlExists(urlString) }) {
+            0 -> false
+            else -> true
+        }
 
     fun deleteUrl(id: Int) {
         mCoroutineScope.launch { mDao.deleteUrl(id) }
@@ -70,9 +92,7 @@ class Repository private constructor(context: Context) {
     }
 
     fun insertUrl(urlItem: UrlEntity) {
-        mCoroutineScope.launch {
-            mDao.insertUrl(urlItem)
-        }
+        mCoroutineScope.launch { mDao.insertUrl(urlItem) }
     }
 
     companion object {
