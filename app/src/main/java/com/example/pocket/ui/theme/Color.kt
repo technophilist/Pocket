d077ade1a0bfe33a9d865313dca9206774a31be0
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-04-25 21:11:34 +0530

    1)Added dependencies required for compose 2)Added 'theme' package to ui.

---
+++ app/src/main/java/com/example/pocket/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.pocket.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+
+//Light Colors
+val Pink100 = Color(0xFFFFF1F1)
+val Pink900 = Color(0xFF3F2C2C)
+val White850 = Color(0x85FFFFFF)
+val Gray = Color(0xFF232323)
+
+//Dark colors
+val Green900 = Color(0xFF2D3B2D)
+val Green300 = Color(0xFFB8C9B8)
+val White150 = Color(0x15FFFFFF)
+
+
+
