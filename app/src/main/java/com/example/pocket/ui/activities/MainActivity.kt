8cc1b5a535e8b67c24bb77808f3f83b245ad0f49
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-05-05 14:05:02 +0530

    1)Added clickable close icon to search text field. 2)Added search filtering (Warning:Filtering is done on the main thread)

--- app/src/main/java/com/example/pocket/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/pocket/ui/activities/MainActivity.kt
@@ -6,19 +6,32 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import com.example.pocket.data.database.UrlEntity
 import com.example.pocket.ui.screens.UrlCard
 import com.example.pocket.ui.theme.PocketAppTheme
 import com.example.pocket.viewmodels.MainScreenViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,6 +53,7 @@ class MainActivity : AppCompatActivity() {
     private fun HomeScreen(viewModel: MainScreenViewModel) {
         val urlItems = viewModel.savedUrls.observeAsState()
         var searchText by remember { mutableStateOf("") }
+        var isSearchTextBoxEnabled by remember{ mutableStateOf(true)}
 
         Column(modifier = Modifier.fillMaxSize()) {
             OutlinedTextField(
@@ -49,13 +63,24 @@ class MainActivity : AppCompatActivity() {
                     .padding(8.dp),
                 value = searchText,
                 onValueChange = { searchText = it },
-                label = { Text(text = "Search...") }
+                label = { Text(text = "Search...") },
+                leadingIcon = { Icon(Icons.Filled.Search, "Search Icon")},
+                trailingIcon = {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            if (isSearchTextBoxEnabled){
+                                searchText = ""
+                                isSearchTextBoxEnabled = false
+                            }
+                        },
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon")
+                },
+                enabled = isSearchTextBoxEnabled,
             )
             urlItems.value?.let {
-                UrlList(
-                    urlItems = it,
-                    onClickItem = { clickedItem -> openUrl(clickedItem.url) }
-                )
+                if (searchText.isBlank()) UrlList(urlItems = it, onClickItem = { clickedItem -> openUrl(clickedItem.url) })
+                else UrlList(urlItems = it.filter { item -> item.contentTitle.contains(searchText,true) }, onClickItem = { clickedItem -> openUrl(clickedItem.url) })
             }
         }
     }
@@ -86,7 +111,6 @@ class MainActivity : AppCompatActivity() {
         val openLinkIntent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(openLinkIntent)
     }
-
 
 }
 
