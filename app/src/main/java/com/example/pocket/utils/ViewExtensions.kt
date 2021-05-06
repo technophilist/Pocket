639202f0144e02d07bac93512748811055f94c5a
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-05-06 13:42:54 +0530

    1)Deleted FragmentExtensions.kt 2)Removed all extension defined for recycler view and search view in   ViewExtensions.kt

--- app/src/main/java/com/example/pocket/utils/ViewExtensions.kt
+++ app/src/main/java/com/example/pocket/utils/ViewExtensions.kt
@@ -7,47 +7,6 @@ import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
 
-inline fun SearchView.doOnTextChanged(crossinline block: (newText: String) -> Unit) {
-    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-        override fun onQueryTextSubmit(query: String?) = false
-        override fun onQueryTextChange(newText: String): Boolean {
-            block(newText)
-            return true
-        }
-    })
-}
-
-enum class RecyclerViewSwipeDirections(val value: Int) {
-    LEFT(ItemTouchHelper.LEFT),
-    RIGHT(ItemTouchHelper.RIGHT),
-    START(ItemTouchHelper.START),
-    END(ItemTouchHelper.END)
-}
-
-inline fun RecyclerView.doOnItemSwiped(
-    swipeDirection: RecyclerViewSwipeDirections,
-    crossinline block: (viewHolder: RecyclerView.ViewHolder, direction: Int) -> Unit
-) {
-    ItemTouchHelper(object : ItemTouchHelper.Callback() {
-        override fun getMovementFlags(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder
-        ) = makeMovementFlags(0, swipeDirection.value)
-
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ) = false
-
-        override fun onSwiped(
-            viewHolder: RecyclerView.ViewHolder,
-            direction: Int
-        ) = block(viewHolder, direction)
-    }).attachToRecyclerView(this)
-
-}
-
 inline fun Snackbar.doOnDismissed(
     crossinline block: (transientBottomBar: Snackbar?, event: Int) -> Unit
 ): Snackbar {
