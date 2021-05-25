e3c30de8e3fb7667a6c2e44312a6e22b965faf2c
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-05-25 20:27:31 +0530

    refactor: Fetch the image url from the open graph tag
    Till this point, the thumbnail image of the bookmarked page was fetched from the
    image tags embedded in it.Now, it has been changed to use the open graph
    tags.

--- app/src/test/java/com/example/pocket/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pocket/ExampleUnitTest.kt
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup
 import org.junit.Assert.*
 import org.junit.Test
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,6 +25,26 @@ class ExampleUnitTest {
         assertNotNull(a)
         val b = kotlin.runCatching { 2/0 }.getOrNull()
         assertNotNull(b)
+    }
+
+    @Test
+    fun imageFetchTest() {
+
+        val document = Jsoup.connect("https://practice.geeksforgeeks.org/courses/dsa-self-paced?gclid=EAIaIQobChMI7Yrl64nk8AIV3oNLBR2MVQufEAAYASAAEgKadPD_BwE").get()
+        val elements = document.select("meta")
+        val ogElements = elements.filter { it.attr("property").contains("og:") }
+        println("===================================================================================")
+       ogElements.forEach{
+           when(it.attr("property")){
+               "og:title" -> { println("title ${it.attr("content")}")}
+               "og:description" -> {println("desc ${it.attr("content")}")}
+               "og:url" -> {println("url ${it.attr("content")}")}
+               "og:image" -> {println("img ${it.attr("content")}")}
+               "og:site_name"->{println("siteName ${it.attr("content")}")}
+           }
+       }
+        println("===================================================================================")
+
     }
 
 
