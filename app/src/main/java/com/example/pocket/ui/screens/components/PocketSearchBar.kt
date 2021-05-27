e23cef019ed80a61032188dccd729464687b9d4e
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-05-27 16:10:53 +0530

    docs: update docs for SearchBar

--- app/src/main/java/com/example/pocket/ui/screens/components/PocketSearchBar.kt
+++ app/src/main/java/com/example/pocket/ui/screens/components/PocketSearchBar.kt
@@ -22,7 +22,13 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 
-
+/**
+ * State for [SearchBar] composable component.
+ * @param focusManager manages the focus of the search bar
+ * @param focusRequester used for monitoring the focus state changes
+ * @param isSearchIconVisible indicates the visibility of the search icon
+ * @param isCloseIconVisible indicates the visibility of the close icon
+ */
 class SearchBarState(
     val focusManager: FocusManager,
     val focusRequester: FocusRequester = FocusRequester(),
@@ -33,6 +39,14 @@ class SearchBarState(
     var isCloseIconVisible by mutableStateOf(isCloseIconVisible)
 }
 
+/**
+ * Utility function for creating a [SearchBarState] with default
+ * behaviour and memoizing it.
+ * @param focusManager manages the focus of the search bar
+ * @param focusRequester used for monitoring the focus state changes
+ * @param isSearchIconVisible indicates the visibility of the search icon
+ * @param isCloseIconVisible indicates the visibility of the close icon
+ */
 @Composable
 fun rememberSearchBarState(
     focusManager: FocusManager = LocalFocusManager.current,
@@ -48,7 +62,30 @@ fun rememberSearchBarState(
     )
 }
 
-
+/**
+ * SearchBar composable is used for displaying a search bar with some useful defaults.
+ *
+ * This component displays a search bar with a leading search icon and trailing close icon.
+ * It is designed in such a way that both of the icons only appear when they are needed.
+ * ie.when the search icon is visible, the close icon wont be visible and vice versa.
+ * When the user taps on the search bar, the search icon automatically disappears,the search
+ * bar is focused and the close icon appears on the screen.When the close icon is tapped,
+ * the focus is cleared and the search icon appears.All default behaviours can be modified
+ * using the [SearchBarState] and the method params.
+ *
+ * @param modifier optional modifier for the search bar
+ * @param searchText the text that is to be displayed in the search bar
+ * @param keyboardActions used to specify actions that will be triggered in response to users
+ * triggering IME action on the software keyboard.
+ * @param onCloseIconClicked the action that is to be done when the close icon is clicked
+ * @param onFocusChanged the action that is to be done when the focus of the search bar is changed
+ * @param onSearchTextChange the action that is to be done when the text in the search bar has
+ * changed
+ * @param state the search bar state
+ * @param textFieldColors colors that will be used to resolve color of the text, content
+ * (including label, placeholder, leading and trailing icons, indicator line) and background for
+ * this text field in different states
+ */
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier,
