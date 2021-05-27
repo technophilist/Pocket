74499aed2607d610f7c00a8b5411f5888e70da68
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-05-27 17:19:16 +0530

    refactor: inject preferences manager to repository

--- app/src/main/java/com/example/pocket/di/AppContainer.kt
+++ app/src/main/java/com/example/pocket/di/AppContainer.kt
@@ -1,11 +1,17 @@
 package com.example.pocket.di
 
 import android.app.Application
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.example.pocket.data.PocketRepository
 import com.example.pocket.data.database.UrlDatabase
 import com.example.pocket.data.network.PocketNetwork
+import com.example.pocket.data.preferences.PocketPreferencesManger
 import kotlinx.coroutines.Dispatchers
 
+
+private const val PREFERENCES_NAME = "pocket_user_preferences"
 class AppContainer(application: Application) {
     private val applicationContext = application.applicationContext
 
@@ -17,6 +23,16 @@ class AppContainer(application: Application) {
     private val database = UrlDatabase.getInstance(applicationContext)
     private val dao = database.getDao()
 
+    //preferences manager
+    private val datastore = PreferenceDataStoreFactory.create{ applicationContext.preferencesDataStoreFile(PREFERENCES_NAME)}
+    private val preferencesManager = PocketPreferencesManger(datastore)
+
     //dependencies
-    val pocketRepository = PocketRepository(network, dao, defaultRepositoryDispatcher, applicationContext)
+    val pocketRepository = PocketRepository(
+        network,
+        dao,
+        preferencesManager,
+        defaultRepositoryDispatcher,
+        applicationContext
+    )
 }
