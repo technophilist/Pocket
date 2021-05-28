d7bc1334d8d2421b204db9abc2c33c4ec45de6ce
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-05-28 16:13:45 +0530

    feat: save the user's app theme prefernence using datastore

--- app/src/main/java/com/example/pocket/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/pocket/ui/activities/MainActivity.kt
@@ -10,10 +10,13 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
+import com.example.pocket.data.preferences.PocketPreferences
 import com.example.pocket.di.PocketApplication
 import com.example.pocket.ui.screens.HomeScreen
 import com.example.pocket.ui.theme.PocketAppTheme
@@ -22,44 +25,62 @@ import com.example.pocket.viewmodels.HomeScreenViewModel
 import com.example.pocket.viewmodels.HomeScreenViewModelImpl
 
 class MainActivity : AppCompatActivity() {
+    private val isDarkModeSupported = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
     private lateinit var mViewModel: HomeScreenViewModel
-
 
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val appContainer = (application as PocketApplication).appContainer
         mViewModel = ViewModelProvider(
             this,
             HomeScreenViewModelFactory(application, appContainer.pocketRepository)
         ).get(HomeScreenViewModelImpl::class.java)
 
-        //TODO Dark mode preferences not saved.If app is closed and reopened,it defaults to white mode
+
+        /*
+         if dark mode is not supported , then the app theme will not automatically
+         change to reflect the night mode colors defined for the status bar in
+         the xml file
+         */
+        if (!isDarkModeSupported) {
+            // observing the current app theme to set the correct status bar color
+            mViewModel.currentAppTheme.observe(this) { theme ->
+                AppCompatDelegate.setDefaultNightMode(
+                    if (theme == PocketPreferences.AppTheme.DARK) AppCompatDelegate.MODE_NIGHT_YES
+                    else AppCompatDelegate.MODE_NIGHT_NO
+                )
+            }
+        }
+
         setContent { PocketApp() }
     }
 
     @ExperimentalMaterialApi
     @Composable
     private fun PocketApp() {
-        var isDarkModeEnabled by rememberSaveable { mutableStateOf(false) }
-        val isDarkModeSupported =
-            remember { android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q }
+        val isDarkModeSupported = remember { this.isDarkModeSupported }
+        val appTheme by mViewModel.currentAppTheme.observeAsState()
+        val isDarkModeEnabled = if (isDarkModeSupported) {
+            /*
+             if the system supports dark mode, use the system's current theme,else
+             observe for changes in the appTheme from the viewModel
+             */
+            isSystemInDarkTheme()
+        } else {
+            (appTheme == PocketPreferences.AppTheme.DARK)
+        }
 
-        PocketAppTheme(
-            if (isDarkModeSupported) isSystemInDarkTheme()
-            else isDarkModeEnabled
-        ) {
+        PocketAppTheme(isDarkModeEnabled) {
             Surface(Modifier.fillMaxSize()) {
                 HomeScreen(
                     viewModel = mViewModel,
                     onClickUrlItem = { openUrl(it.url) },
                     isDarkModeSupported = isDarkModeSupported,
                     onDarkModeIconClicked = {
-                        isDarkModeEnabled = !isDarkModeEnabled
-                        AppCompatDelegate.setDefaultNightMode(
-                            if (isDarkModeEnabled) AppCompatDelegate.MODE_NIGHT_YES
-                            else AppCompatDelegate.MODE_NIGHT_NO
+                        mViewModel.changeAppTheme(
+                            if (appTheme == PocketPreferences.AppTheme.LIGHT) PocketPreferences.AppTheme.DARK
+                            else PocketPreferences.AppTheme.LIGHT
                         )
                     },
                     isDarkModeEnabled = isDarkModeEnabled
