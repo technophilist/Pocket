15abf9be62ca079ac97314eebc8a37b21b2294b2
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-06-01 11:51:46 +0530

    fix: remove inappropriate blocking calls associated with glide
    Removed inapproproate blocking calls from within the coroutines that was
    caused by glide's get() method by adding a new extension suspend function that does not block
    the thread that the coroutine is currently running on.

---
+++ app/src/main/java/com/example/pocket/utils/GlideExtensions.kt
@@ -0,0 +1,29 @@
+package com.example.pocket.utils
+
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+/**
+ * This method is used for getting the resource that was downloaded by glide
+ * @param TranscodeType the type of the result(file) that is to be downloaded
+ */
+suspend fun <TranscodeType> RequestBuilder<TranscodeType>.getDownloadedResource(): TranscodeType {
+    return suspendCancellableCoroutine { cancellableContinuation ->
+        this.into(object : CustomTarget<TranscodeType>() {
+            override fun onResourceReady(
+                resource: TranscodeType,
+                transition: Transition<in TranscodeType>?
+            ) {
+                //resuming the coroutine when this method is invoked
+                cancellableContinuation.resume(resource)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {}
+
+        })
+    }
+}
