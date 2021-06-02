4d09f0cf4bc1ab8c017499beb6208fd30aa868ff
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-06-02 19:49:28 +0530

    refactor!: change all urlString parameter calls to URL object
    BREAKING CHANGE: all objects now use the URL object in place of url
    string.This is a major breaking change since this change breaks all
    methods that use string to represent the url.

--- app/src/main/java/com/example/pocket/workers/SaveUrlWorker.kt
+++ app/src/main/java/com/example/pocket/workers/SaveUrlWorker.kt
@@ -6,6 +6,7 @@ import androidx.work.WorkerParameters
 import com.example.pocket.data.network.PocketNetwork
 import com.example.pocket.di.PocketApplication
 import com.example.pocket.ui.activities.HandleUrlActivity
+import java.net.URL
 
 /**
  * This worker is triggered whenever the user saves the url
@@ -18,7 +19,7 @@ class SaveUrlWorker(
     override suspend fun doWork() =
         inputData.getString(HandleUrlActivity.EXTRA_URL)?.let {
             val repository = (applicationContext as PocketApplication).appContainer.pocketRepository
-            repository.saveUrl(it)
+            repository.saveUrl(URL(it))
             Result.success()
         } ?: Result.failure()
 }
