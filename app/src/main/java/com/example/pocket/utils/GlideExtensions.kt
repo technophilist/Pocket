865ab690ea3d58cae8581bb9cb9b0baedfb8c515
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-06-04 22:49:21 +0530

    refactor: add resumeWithException block to getDownloadedResource
    The getDownloadedResource() extension function will now resume with an
    exception when onLoadFailed() of CustomTarget() is invoked.

--- app/src/main/java/com/example/pocket/utils/GlideExtensions.kt
+++ app/src/main/java/com/example/pocket/utils/GlideExtensions.kt
@@ -2,10 +2,12 @@ package com.example.pocket.utils
 
 import android.graphics.drawable.Drawable
 import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 /**
  * This method is used for getting the resource that was downloaded by glide
@@ -24,6 +26,9 @@ suspend fun <TranscodeType> RequestBuilder<TranscodeType>.getDownloadedResource(
 
             override fun onLoadCleared(placeholder: Drawable?) {}
 
+            override fun onLoadFailed(errorDrawable: Drawable?) {
+                 cancellableContinuation.resumeWithException(GlideException("Error fetching the image"))
+            }
         })
     }
 }
