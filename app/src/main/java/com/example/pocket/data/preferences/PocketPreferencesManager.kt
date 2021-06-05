f44cb305abd2afac81b6ee135c1ffdda57f58ff7
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-06-06 00:04:18 +0530

    refactor: add exception handling to datastore

--- app/src/main/java/com/example/pocket/data/preferences/PocketPreferencesManager.kt
+++ app/src/main/java/com/example/pocket/data/preferences/PocketPreferencesManager.kt
@@ -1,9 +1,13 @@
 package com.example.pocket.data.preferences
 
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.pocket.data.preferences.PocketPreferencesManger.PreferenceKeys.DEVICE_THEME
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 
 class PocketPreferencesManger(
@@ -13,17 +17,30 @@ class PocketPreferencesManger(
     private object PreferenceKeys {
         val DEVICE_THEME = stringPreferencesKey("device_theme_preference")
     }
+    
+    override val userPreferences = mDataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                /*
+                if an IOException occurs while reading data,return a User preferences
+                object with default value
+                */
+                UserPreferences(PocketPreferences.AppTheme.SYSTEM)
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            val deviceTheme = PocketPreferences.AppTheme.valueOf(
+                preferences[DEVICE_THEME] ?: PocketPreferences.AppTheme.SYSTEM.name
+            )
+            UserPreferences(deviceTheme)
+        }
 
-    //TODO must handle errors
-    override val userPreferences = mDataStore.data.map { preferences ->
-        val deviceTheme = PocketPreferences.AppTheme.valueOf(preferences[DEVICE_THEME] ?: PocketPreferences.AppTheme.SYSTEM.name)
-        UserPreferences(deviceTheme)
-    }
-
-    override suspend fun updateThemePreference(appTheme: PocketPreferences.AppTheme){
-       mDataStore.edit {preferences->
-           preferences[DEVICE_THEME] = appTheme.name
-       }
+    override suspend fun updateThemePreference(appTheme: PocketPreferences.AppTheme) {
+        mDataStore.edit { preferences ->
+            preferences[DEVICE_THEME] = appTheme.name
+        }
     }
 }
 
