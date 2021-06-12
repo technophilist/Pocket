778dfdab79144274dbbf2b0f7e4296caf353ab58
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-06-12 17:50:19 +0530

    refactor: remove getInstance() from UrlDatabase
    Dependency injection eliminates the need for having a getInstance singleton
    method.

--- app/src/main/java/com/example/pocket/data/database/UrlDatabase.kt
+++ app/src/main/java/com/example/pocket/data/database/UrlDatabase.kt
@@ -1,24 +1,9 @@
 package com.example.pocket.data.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [UrlEntity::class], version = 1, exportSchema = false)
 abstract class UrlDatabase : RoomDatabase() {
-    companion object {
-        private var mInstance: UrlDatabase? = null
-        fun getInstance(context: Context) = mInstance ?: synchronized(this) {
-            mInstance = Room.databaseBuilder(
-                context.applicationContext,
-                UrlDatabase::class.java,
-                "Pocket_Database"
-            ).build()
-            mInstance!!
-        }
-
-    }
-
     abstract fun getDao(): Dao
 }
--- app/src/main/java/com/example/pocket/di/AppContainer.kt
+++ app/src/main/java/com/example/pocket/di/AppContainer.kt
@@ -1,9 +1,9 @@
 package com.example.pocket.di
 
 import android.app.Application
-import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.room.Room
 import com.example.pocket.data.PocketRepository
 import com.example.pocket.data.database.UrlDatabase
 import com.example.pocket.data.network.PocketNetwork
@@ -12,19 +12,29 @@ import kotlinx.coroutines.Dispatchers
 
 
 private const val PREFERENCES_NAME = "pocket_user_preferences"
+private const val DATABASE_NAME = "Pocket_Database"
+
 class AppContainer(application: Application) {
+
     private val applicationContext = application.applicationContext
 
     //default Dispatchers
     private val defaultRepositoryDispatcher = Dispatchers.IO
     private val defaultNetworkDispatcher = Dispatchers.IO
 
+    //data
     private val network = PocketNetwork(applicationContext, defaultNetworkDispatcher)
-    private val database = UrlDatabase.getInstance(applicationContext)
+    private val database = Room.databaseBuilder(
+        applicationContext,
+        UrlDatabase::class.java,
+        DATABASE_NAME
+    ).build()
     private val dao = database.getDao()
 
     //preferences manager
-    private val datastore = PreferenceDataStoreFactory.create{ applicationContext.preferencesDataStoreFile(PREFERENCES_NAME)}
+    private val datastore = PreferenceDataStoreFactory.create {
+        applicationContext.preferencesDataStoreFile(PREFERENCES_NAME)
+    }
     private val preferencesManager = PocketPreferencesManger(datastore)
 
     //dependencies
