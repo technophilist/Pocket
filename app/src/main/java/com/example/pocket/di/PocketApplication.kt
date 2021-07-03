f5c8c48ea3835926782aed186ae3b4c9b13b31a9
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-03 13:12:38 +0530

    refactor: instantiate timber dependency in PocketApplication

--- app/src/main/java/com/example/pocket/di/PocketApplication.kt
+++ app/src/main/java/com/example/pocket/di/PocketApplication.kt
@@ -1,13 +1,16 @@
 package com.example.pocket.di
 
 import android.app.Application
+import com.example.pocket.BuildConfig
+import timber.log.Timber
 
-class PocketApplication:Application() {
+class PocketApplication : Application() {
 
     lateinit var appContainer: AppContainer
 
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         appContainer = AppContainer(this)
     }
 
