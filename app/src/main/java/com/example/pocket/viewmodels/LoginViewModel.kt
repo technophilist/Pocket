502a8815c59b7e1e8440a90dee513c084c3bb121
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-06 16:45:32 +0530

    fix: change username param to emailAddress in authenticate method

--- app/src/main/java/com/example/pocket/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/pocket/viewmodels/LoginViewModel.kt
@@ -2,14 +2,14 @@ package com.example.pocket.viewmodels
 
 
 interface LoginViewModel {
-    fun authenticate(userName:String,password:String): Boolean
+    fun authenticate(emailAddress:String, password:String): Boolean
 
 }
 
 class LoginViewModelImpl : LoginViewModel {
 
 
-    override fun authenticate(userName: String, password: String): Boolean {
+    override fun authenticate(emailAddress: String, password: String): Boolean {
         TODO()
 
     }
