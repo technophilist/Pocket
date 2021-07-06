7c00fcb6b6124e00a27c8b8cf91edea65514bb1c
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-07-06 16:33:49 +0530

    refactor: change remember to rememberSaveable
    This commit changes the call to remember() in
    PocketSearchBar.rememberSearchBarState() to rememberSaveable().This
    will ensure that the instance of the SearchBarState will survive
    configuration changes.

--- app/src/main/java/com/example/pocket/ui/screens/components/PocketSearchBar.kt
+++ app/src/main/java/com/example/pocket/ui/screens/components/PocketSearchBar.kt
@@ -1,26 +1,20 @@
 package com.example.pocket.ui.screens.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.*
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.VisualTransformation
 
 /**
  * State for [SearchBar] composable component.
@@ -53,7 +47,7 @@ fun rememberSearchBarState(
     focusRequester: FocusRequester = FocusRequester(),
     isSearchIconVisible: Boolean = true,
     isCloseIconVisible: Boolean = false
-) = remember {
+) = rememberSaveable {
     SearchBarState(
         focusManager = focusManager,
         focusRequester = focusRequester,
@@ -92,7 +86,7 @@ fun SearchBar(
     searchText: String,
     onSearchTextChange: (String) -> Unit,
     state: SearchBarState = rememberSearchBarState(),
-    textFieldColors:TextFieldColors = TextFieldDefaults.textFieldColors(),
+    textFieldColors: TextFieldColors = TextFieldDefaults.textFieldColors(),
     keyboardActions: KeyboardActions = KeyboardActions(onSearch = {
         if (searchText.isBlank()) {
             state.isSearchIconVisible = true
@@ -127,7 +121,7 @@ fun SearchBar(
         }
     }
 
-   TextField(
+    TextField(
         modifier = modifier
             .focusRequester(state.focusRequester)
             .onFocusChanged { onFocusChanged(it) },
