d183038fac4662b058149bd3aaf2bfb10cc5c8e1
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-06 12:57:31 +0530

    refactor: apply state hoisting to VectorArtCarousel

--- app/src/main/java/com/example/pocket/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/pocket/ui/screens/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.pocket.ui.screens
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -22,9 +23,40 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 
+private data class VectorArtCard(
+    @DrawableRes val id: Int,
+    val title: String,
+    val description: String,
+    val imageDescription: String
+)
+
 @ExperimentalPagerApi
 @Composable
-fun WelcomeScreen(isDarkMode: Boolean = false) {
+fun WelcomeScreen() {
+    val vectorArtCards = listOf(
+        VectorArtCard(
+            R.drawable.welcome_screen_save_files,
+            title = "Save what really interests you",
+            description = "Save articles,videos or any online content you like. " +
+                    "Add them to Pocket and read them when you have time.",
+            imageDescription = ""
+        ),
+        VectorArtCard(
+            id = R.drawable.welcome_screen_read_peacefully_vector_art,
+            title = "Your quiet corner of the internet",
+            description = "Pocket saves articles in a clean layout designed for reading-" +
+                    "no interruptions, no popups-so you can sidestep the internet's noise.",
+            imageDescription = ""
+        ),
+        VectorArtCard(
+            id = R.drawable.welcome_screen_dark_mode_vector_art,
+            title = "Reduce eye strain with dark mode",
+            description = "Want to dim the lights? Don't let a bright screen mess with your eyes or sleep. " +
+                    "Turn on dark mode and continue your reading.",
+            imageDescription = ""
+        )
+    )
+
     PocketAppTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
             Column(
@@ -46,7 +78,8 @@ fun WelcomeScreen(isDarkMode: Boolean = false) {
                 VectorArtCarousel(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .fillMaxHeight(0.8f)
+                        .fillMaxHeight(0.8f),
+                    vectorArtCards = vectorArtCards
                 )
 
                 Button(
@@ -74,49 +107,48 @@ fun WelcomeScreen(isDarkMode: Boolean = false) {
 
 @ExperimentalPagerApi
 @Composable
-fun VectorArtCarousel(modifier: Modifier = Modifier) {
+private fun VectorArtCarousel(modifier: Modifier = Modifier, vectorArtCards: List<VectorArtCard>) {
 
     val pagerState = rememberPagerState(
-        pageCount = 3,
+        pageCount = vectorArtCards.size,
         infiniteLoop = true,
         initialOffscreenLimit = 2,
     )
 
-    val page1 = getVectorArtPage(
-        painter = painterResource(id = R.drawable.welcome_screen_save_files),
-        title = "Save what really interests you",
-        description = "Save articles,videos or any online content you like. " +
-                "Add them to Pocket and read them when you have time.",
-        imageDescription = ""
-    )
-
-
-    val page2 = getVectorArtPage(
-        painter = painterResource(id = R.drawable.welcome_screen_read_peacefully_vector_art),
-        title = "Your quiet corner of the internet",
-        description = "Pocket saves articles in a clean layout designed for reading-" +
-                "no interruptions, no popups-so you can sidestep the internet's noise.",
-        imageDescription = ""
-    )
-
-    val page3 = getVectorArtPage(
-        painter = painterResource(id = R.drawable.welcome_screen_dark_mode_vector_art),
-        title = "Reduce eye strain with dark mode",
-        description = "Want to dim the lights? Don't let a bright screen mess with your eyes or sleep. " +
-                "Turn on dark mode and continue your reading.",
-        imageDescription = ""
-    )
     Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
 
         HorizontalPager(
             modifier = Modifier.align(Alignment.CenterHorizontally),
             state = pagerState,
         ) { page ->
-            when (page) {
-                0 -> page1()
-                1 -> page2()
-                2 -> page3()
-                else -> throw IllegalStateException()
+            Column(
+                modifier = modifier,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = vectorArtCards[page].id),
+                    contentDescription = vectorArtCards[page].imageDescription
+                )
+
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    text = vectorArtCards[page].title,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 32.dp, end = 32.dp),
+                    text = vectorArtCards[page].description,
+                    textAlign = TextAlign.Center
+                )
+
             }
         }
 
@@ -125,43 +157,6 @@ fun VectorArtCarousel(modifier: Modifier = Modifier) {
                 .padding(top = 40.dp)
                 .align(Alignment.CenterHorizontally),
             pagerState = pagerState
-        )
-
-    }
-}
-
-
-@Composable
-private fun getVectorArtPage(
-    modifier: Modifier = Modifier,
-    painter: Painter,
-    title: String,
-    description: String,
-    imageDescription: String,
-): @Composable () -> Unit = @Composable {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Image(painter = painter, contentDescription = imageDescription)
-
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp),
-            text = title,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
-
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 32.dp, end = 32.dp),
-            text = description,
-            textAlign = TextAlign.Center
         )
 
     }
