69fce6a733647266ba7094f1e2d865cb0b4e60c7
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-06 22:57:04 +0530

    refactor: add password param to createAccount method
    This commit adds a password parameter to the create account method of
    the AuthenticationService interface.

--- app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
+++ app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
@@ -2,7 +2,11 @@ package com.example.pocket.auth
 
 interface AuthenticationService {
     suspend fun signIn(email: String, password: String):AuthenticationResult
-    suspend fun createAccount(email: String, password: String):AuthenticationResult
+    suspend fun createAccount(
+        username:String,
+        email: String,
+        password: String,
+    ):AuthenticationResult
 }
 
 sealed class AuthenticationResult {
