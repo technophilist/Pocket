18fc9f047d86973bcf428eb6b7feca95eb8a2228
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-07-07 12:36:27 +0530

    refactor: add isLoggedIn and signOut() to authentication service

--- app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
+++ app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
@@ -3,17 +3,19 @@ package com.example.pocket.auth
 import android.net.Uri
 
 interface AuthenticationService {
-    suspend fun signIn(email: String, password: String):AuthenticationResult
+    val isLoggedIn: Boolean
+    suspend fun signIn(email: String, password: String): AuthenticationResult
     suspend fun createAccount(
-        username:String,
+        username: String,
         email: String,
         password: String,
         profilePhotoUri: Uri?
-    ):AuthenticationResult
+    ): AuthenticationResult
+    suspend fun signOut()
 }
 
 sealed class AuthenticationResult {
-    data class Success(val user: User) : AuthenticationResult()
+    data class Success(val user: PocketUser) : AuthenticationResult()
     data class Failure(val exception: Exception) : AuthenticationResult()
 }
 
