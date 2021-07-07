989a11dba340131e6a295aebdb625c55e1064fab
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-07-07 23:48:57 +0530

    refactor: add login viewModel factory

--- app/src/main/java/com/example/pocket/utils/ViewModelFactories.kt
+++ app/src/main/java/com/example/pocket/utils/ViewModelFactories.kt
@@ -3,8 +3,12 @@ package com.example.pocket.utils
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.pocket.auth.AuthenticationService
 import com.example.pocket.data.Repository
 import com.example.pocket.viewmodels.HomeScreenViewModelImpl
+import com.example.pocket.viewmodels.LoginViewModel
+import com.example.pocket.viewmodels.LoginViewModelImpl
+import kotlinx.coroutines.CoroutineDispatcher
 
 
 class HomeScreenViewModelFactory(
@@ -13,4 +17,13 @@ class HomeScreenViewModelFactory(
 ) : ViewModelProvider.AndroidViewModelFactory(application) {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>) = HomeScreenViewModelImpl(repository,application) as T
+}
+
+
+class LoginViewModelFactory(
+    private val authenticationService: AuthenticationService,
+    private val defaultDispatcher:CoroutineDispatcher
+):ViewModelProvider.Factory{
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T = LoginViewModelImpl(authenticationService,defaultDispatcher) as T
 }
