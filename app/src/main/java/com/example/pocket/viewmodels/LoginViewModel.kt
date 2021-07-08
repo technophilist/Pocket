fa320c6d6b396ae2c1f0266696577599fcfcea92
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-07-08 16:06:14 +0530

    fix: remove password and email validation for viewmodel
    This commit removes the password and email validation methods from the
    login viewmodel because the login viewmodel is only responsible for
    authenticating, not to check the validity of the stucture of the email and password.

--- app/src/main/java/com/example/pocket/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/pocket/viewmodels/LoginViewModel.kt
@@ -14,12 +14,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-/**
- * These two classes are custom [Exception] classes that will be used
- * in the [LoginViewModel.authenticationResult].
- */
-class InvalidEmailException(message: String? = null) : Exception(message)
-class InvalidPasswordException(message: String? = null) : Exception(message)
 
 interface LoginViewModel {
     val authenticationResult: LiveData<AuthenticationResult>
@@ -38,44 +32,14 @@ class LoginViewModelImpl(
      * authentication is successful, it returns an instance of [AuthenticationResult.Success]
      * with the authenticated user in it. Else, it returns an instance of [AuthenticationResult.Failure]
      * with the exception that caused the failure.
-     *
-     * One major use-case is to check whether the the exception was cause by an invalid username or password.
-     * Example usage to get check whether the exception was cause by an invalid password or username:
-     * ```
-     * when (authenticationResult.exception) {
-     *   is InvalidEmailException -> { //Do something }
-     *   is InvalidPasswordException -> { //Do something }
-     *  }
-     *```
      */
     override val authenticationResult = _authenticationResult
-
-    /**
-     * The method is used to check whether the [email] is valid .An email is valid
-     * if, and only if, it is not blank(ie. is not empty and doesn't contain whitespace characters)
-     * and matches the [Patterns.EMAIL_ADDRESS] regex.
-     */
-    private fun isValidEmail(email: String) =
-        email.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
-
-    /**
-     * The method is used to check whether the [password] is valid.A password is valid if, and only if,
-     * it is of length 8 , contains atleast one uppercase and lowercase letter and contains atleast one digit.
-     */
-    private fun isValidPassword(
-        password: String
-    ) =
-        password.length == 8 && password.containsUppercase() && password.containsLowercase() && password.containsDigit()
 
     /**
      * This method is used to authenticate a user with the provided [emailAddress] and [password].
      */
     override fun authenticate(emailAddress: String, password: String) {
-        if (!isValidEmail(emailAddress))
-            _authenticationResult.value = AuthenticationResult.Failure(InvalidEmailException())
-        else if (!isValidPassword(password))
-            _authenticationResult.value = AuthenticationResult.Failure(InvalidPasswordException())
-        else CoroutineScope(mDefaultDispatcher).launch {
+      CoroutineScope(mDefaultDispatcher).launch {
             //if the email and password are valid, then try signing in the user with the authentication service.
             _authenticationResult.postValue(mAuthenticationService.signIn(emailAddress, password))
         }
