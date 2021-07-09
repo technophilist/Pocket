8bb89d49a093ccb54f1cfeacc94c8490a96426a8
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-07-09 13:43:05 +0530

    feat: add loading animation to login screen

--- app/src/main/java/com/example/pocket/ui/screens/LoginScreeen.kt
+++ app/src/main/java/com/example/pocket/ui/screens/LoginScreeen.kt
@@ -10,6 +10,8 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -24,7 +26,6 @@ import com.example.pocket.viewmodels.LoginViewModel
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
-
 @Composable
 fun LoginScreen(
     viewmodel: LoginViewModel,
@@ -35,6 +36,7 @@ fun LoginScreen(
     var emailAddressText by remember { mutableStateOf("") }
     var passwordText by remember { mutableStateOf("") }
     var isPasswordVisible by remember { mutableStateOf(false) }
+    var isLoading by remember { mutableStateOf(false) }
 
     /*
      * Initially these values will be true in order to not display
@@ -51,6 +53,7 @@ fun LoginScreen(
          */
         when (val result = authenticationResult.value) {
             is AuthenticationResult.Success -> {
+                isLoading = false
                 navController.navigate(NavigationDestinations.HOME_SCREEN.navigationString) {
                     //if successfully logged in, pop the backstack
                     popUpTo(NavigationDestinations.WELCOME_SCREEN.navigationString) {
@@ -59,17 +62,21 @@ fun LoginScreen(
                 }
             }
             is AuthenticationResult.Failure -> {
+                isLoading = false
                 /*
                  * if the credentials are invalid or if the user is not found set
                  * isCredentialsValid to false.
                  */
                 if (
                     result.exception is FirebaseAuthInvalidCredentialsException ||
-                    result.exception is FirebaseAuthInvalidUserException
+                    result.exception is FirebaseAuthInvalidUserException ||
+                    result.exception is IllegalArgumentException
                 ) isCredentialsValid = false
             }
         }
-        onDispose {}
+        onDispose {
+            isLoading = true
+        }
     }
 
     val termsAndConditionText = buildAnnotatedString {
@@ -87,87 +94,105 @@ fun LoginScreen(
 
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
             modifier = Modifier
-                .paddingFromBaseline(top = 184.dp),
-            text = "Log in with mail",
-            style = MaterialTheme.typography.h1
-        )
+                .fillMaxSize()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .paddingFromBaseline(top = 184.dp),
+                text = "Log in with mail",
+                style = MaterialTheme.typography.h1
+            )
 
-        Spacer(modifier = Modifier.padding(16.dp))
+            Spacer(modifier = Modifier.padding(16.dp))
 
-        OutlinedTextField(
-            modifier = Modifier
-                .height(56.dp)
-                .fillMaxWidth(),
-            value = emailAddressText,
-            onValueChange = { emailAddressText = it },
-            placeholder = { Text(text = "Email Address") },
-            textStyle = MaterialTheme.typography.body1,
-            isError = !isCredentialsValid,
-            maxLines = 1
-        )
+            OutlinedTextField(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth(),
+                value = emailAddressText,
+                onValueChange = { emailAddressText = it },
+                placeholder = { Text(text = "Email Address") },
+                textStyle = MaterialTheme.typography.body1,
+                isError = !isCredentialsValid,
+                maxLines = 1
+            )
 
-        Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
 
-        OutlinedTextField(
-            modifier = Modifier
-                .height(56.dp)
-                .fillMaxWidth(),
-            value = passwordText,
-            onValueChange = { passwordText = it },
-            placeholder = { Text(text = "Password") },
-            textStyle = MaterialTheme.typography.body1,
-            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            isError = !isCredentialsValid,
-            trailingIcon = {
-                Icon(
-                    modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible },
-                    imageVector = if (isPasswordVisible) Icons.Filled.Visibility
-                    else Icons.Filled.VisibilityOff,
-                    contentDescription = ""
+            OutlinedTextField(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth(),
+                value = passwordText,
+                onValueChange = { passwordText = it },
+                placeholder = { Text(text = "Password") },
+                textStyle = MaterialTheme.typography.body1,
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                isError = !isCredentialsValid,
+                trailingIcon = {
+                    Icon(
+                        modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible },
+                        imageVector = if (isPasswordVisible) Icons.Filled.Visibility
+                        else Icons.Filled.VisibilityOff,
+                        contentDescription = ""
+                    )
+                }
+            )
+
+            if (!isCredentialsValid) {
+                Text(
+                    modifier = Modifier.align(Alignment.Start),
+                    text = "The email address or password that you've entered is incorrect. Please check the credentials.",
+                    color = MaterialTheme.colors.error
                 )
             }
-        )
 
-        if (!isCredentialsValid) {
+            Spacer(modifier = Modifier.padding(8.dp))
+
             Text(
-                modifier = Modifier.align(Alignment.Start),
-                text = "The email address or password that you've entered is incorrect. Please check the credentials.",
-                color = MaterialTheme.colors.error
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .paddingFromBaseline(top = 24.dp),
+                text = termsAndConditionText,
+                style = MaterialTheme.typography.body2
+            )
+
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            Button(
+                modifier = Modifier
+                    .height(48.dp)
+                    .fillMaxWidth(),
+                onClick = {
+                    isLoading = true
+                    viewmodel.authenticate(emailAddressText, passwordText)
+                },
+                shape = MaterialTheme.shapes.medium,
+                content = { Text(text = "Log in", fontWeight = FontWeight.Bold) }
             )
         }
 
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        Text(
+        if (isLoading) LoadingProgressOverlay(
             modifier = Modifier
-                .fillMaxWidth()
-                .paddingFromBaseline(top = 24.dp),
-            text = termsAndConditionText,
-            style = MaterialTheme.typography.body2
+                .fillMaxSize()
+                .alpha(0.5f)
         )
-
-        Spacer(modifier = Modifier.padding(16.dp))
-
-        Button(
-            modifier = Modifier
-                .height(48.dp)
-                .fillMaxWidth(),
-            onClick = {
-                viewmodel.authenticate(emailAddressText, passwordText)
-
-            },
-            shape = MaterialTheme.shapes.medium,
-            content = { Text(text = "Log in", fontWeight = FontWeight.Bold) }
-        )
-
     }
+}
 
+@Composable
+private fun LoadingProgressOverlay(modifier: Modifier) {
+    Surface(
+        modifier = modifier,
+        color = Color.Black
+    ) {
+        Box {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
 }
