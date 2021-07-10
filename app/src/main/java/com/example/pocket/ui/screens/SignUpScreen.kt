2a0616c1ebeae87c9beb26e7ac9be84cbc50f3b1
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-10 14:04:06 +0530

    refactor: add visual transformation to password textfield in signUp screen

--- app/src/main/java/com/example/pocket/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/pocket/ui/screens/SignUpScreen.kt
@@ -1,16 +1,19 @@
 package com.example.pocket.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -22,6 +25,9 @@ fun SignUpScreen(navController: NavController) {
     var passwordText by remember { mutableStateOf("") }
     var firstNameText by remember { mutableStateOf("") }
     var lastNameText by remember { mutableStateOf("") }
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
 
     val termsAndConditionText = buildAnnotatedString {
         append("By clicking below you agree to our ")
@@ -99,7 +105,16 @@ fun SignUpScreen(navController: NavController) {
             value = passwordText,
             onValueChange = { passwordText = it },
             placeholder = { Text(text = "Password") },
-            textStyle = MaterialTheme.typography.body1
+            textStyle = MaterialTheme.typography.body1,
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                Icon(
+                    modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible },
+                    imageVector = if (isPasswordVisible) Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff,
+                    contentDescription = ""
+                )
+            }
         )
 
         Text(
