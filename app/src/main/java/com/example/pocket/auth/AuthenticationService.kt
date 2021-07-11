2744b80ae3a4bb48c1071d869de47e7782b4b410
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-11 23:03:14 +0530

    refactor: create AuthenticationServiceException enum

--- app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
+++ app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
@@ -1,6 +1,7 @@
 package com.example.pocket.auth
 
 import android.net.Uri
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 interface AuthenticationService {
     val isLoggedIn: Boolean
@@ -11,12 +12,24 @@ interface AuthenticationService {
         password: String,
         profilePhotoUri: Uri? = null
     ): AuthenticationResult
+
     suspend fun signOut()
 }
 
 sealed class AuthenticationResult {
     data class Success(val user: PocketUser) : AuthenticationResult()
-    data class Failure(val exception: Exception) : AuthenticationResult()
+    data class Failure(val exception: AuthenticationServiceException) : AuthenticationResult()
+}
+
+enum class AuthenticationServiceException(
+    val errorMessage: String? = null,
+    val cause: Throwable? = null
+) {
+    AuthServiceInvalidCredentialsException("Invalid Credentials"),
+    AuthServiceUserCollisionException("A user with the same credentials already exists"),
+    AuthServiceAccountCreationException(),
+    AuthServiceInvalidUserException("This user doesn't exist"),
+    AuthServiceWeakPasswordException("Weak Password")
 }
 
 
