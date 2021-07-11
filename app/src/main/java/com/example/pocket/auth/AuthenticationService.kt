99c7ab65b0f5e0835284670dfd402ea2ac236005
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-07-11 23:10:48 +0530

    Revert "refactor: create AuthenticationServiceException enum"
    This reverts commit 30f90632056216d405919a527d47b4a78a310f20 because it
    contained an unintended change wherein the
    AuthenticationResult.Failure's exception attribute datatype was changed
    to AuthenticationServiceException enum instead of Exception which caused
    a compile error.

--- app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
+++ app/src/main/java/com/example/pocket/auth/AuthenticationService.kt
@@ -1,7 +1,6 @@
 package com.example.pocket.auth
 
 import android.net.Uri
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 interface AuthenticationService {
     val isLoggedIn: Boolean
@@ -12,24 +11,12 @@ interface AuthenticationService {
         password: String,
         profilePhotoUri: Uri? = null
     ): AuthenticationResult
-
     suspend fun signOut()
 }
 
 sealed class AuthenticationResult {
     data class Success(val user: PocketUser) : AuthenticationResult()
-    data class Failure(val exception: AuthenticationServiceException) : AuthenticationResult()
-}
-
-enum class AuthenticationServiceException(
-    val errorMessage: String? = null,
-    val cause: Throwable? = null
-) {
-    AuthServiceInvalidCredentialsException("Invalid Credentials"),
-    AuthServiceUserCollisionException("A user with the same credentials already exists"),
-    AuthServiceAccountCreationException(),
-    AuthServiceInvalidUserException("This user doesn't exist"),
-    AuthServiceWeakPasswordException("Weak Password")
+    data class Failure(val exception: Exception) : AuthenticationResult()
 }
 
 
