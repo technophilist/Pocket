15dd26541d8518b5567b0b399191b9fb2bf3adb6
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-12 20:33:31 +0530

    feat: add loading overlay to signUpScreen

--- app/src/main/java/com/example/pocket/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/pocket/ui/screens/SignUpScreen.kt
@@ -1,6 +1,5 @@
 package com.example.pocket.ui.screens
 
-import android.hardware.biometrics.BiometricPrompt
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -19,30 +18,36 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import com.example.pocket.auth.*
+import com.example.pocket.auth.AuthServiceInvalidEmailException
+import com.example.pocket.auth.AuthServiceInvalidPasswordException
+import com.example.pocket.auth.AuthServiceUserCollisionException
+import com.example.pocket.auth.AuthenticationResult
 import com.example.pocket.di.AppContainer
 import com.example.pocket.di.SignUpContainer
 import com.example.pocket.ui.navigation.NavigationDestinations
+import com.example.pocket.ui.screens.components.CircularLoadingProgressOverlay
 import com.example.pocket.viewmodels.SignUpViewModelImpl
 
 @Composable
 fun SignUpScreen(
-    appContainer:AppContainer,
+    appContainer: AppContainer,
     navController: NavController
 ) {
 
-    val viewmodel = remember{
+    val viewmodel = remember {
         //start sign-up flow
         appContainer.signUpContainer = SignUpContainer()
         appContainer.signUpContainer!!.signUpViewModelFactory.create(SignUpViewModelImpl::class.java)
     }
     val result = viewmodel.accountCreationResult.observeAsState()
-    var isErrorMessageVisible by remember{
+    var isErrorMessageVisible by remember {
         mutableStateOf(false)
     }
-    var errorMessage by remember{
+    var errorMessage by remember {
         mutableStateOf("")
+    }
+    var isLoading by remember {
+        mutableStateOf(false)
     }
     var emailAddressText by remember { mutableStateOf("") }
     var passwordText by remember { mutableStateOf("") }
@@ -68,19 +73,20 @@ fun SignUpScreen(
     }
 
     DisposableEffect(result.value) {
-        when(val authResult = result.value){
-            is AuthenticationResult.Success->{
+        isLoading = false
+        when (val authResult = result.value) {
+            is AuthenticationResult.Success -> {
                 isErrorMessageVisible = false
                 //end sign-up flow
                 appContainer.signUpContainer = null
-                navController.navigate(NavigationDestinations.HOME_SCREEN.navigationString){
-                    popUpTo(NavigationDestinations.WELCOME_SCREEN.navigationString){
+                navController.navigate(NavigationDestinations.HOME_SCREEN.navigationString) {
+                    popUpTo(NavigationDestinations.WELCOME_SCREEN.navigationString) {
                         inclusive = true
                     }
                 }
             }
             is AuthenticationResult.Failure -> {
-               errorMessage =  when(authResult.authServiceException){
+                errorMessage = when (authResult.authServiceException) {
                     is AuthServiceInvalidEmailException -> "Please enter a valid email."
                     is AuthServiceInvalidPasswordException -> "The password must be of length 8, and must contain atleast one uppercase and lowercase letter and atleast one digit."
                     is AuthServiceUserCollisionException -> "A user with the same email already exists."
@@ -92,109 +98,117 @@ fun SignUpScreen(
 
         onDispose {
             isErrorMessageVisible = false
+            isLoading = true
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Text(
+    CircularLoadingProgressOverlay(isOverlayVisible = isLoading) {
+        Column(
             modifier = Modifier
-                .paddingFromBaseline(top = 184.dp),
-            text = "Sign up for a new account",
-            style = MaterialTheme.typography.h1
-        )
+                .fillMaxSize()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        Spacer(modifier = Modifier.padding(16.dp))
+            Text(
+                modifier = Modifier
+                    .paddingFromBaseline(top = 184.dp),
+                text = "Sign up for a new account",
+                style = MaterialTheme.typography.h1
+            )
 
-        Row(modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+
+                OutlinedTextField(
+                    modifier = Modifier
+                        .height(56.dp)
+                        .fillMaxWidth(0.5f),
+                    value = firstNameText,
+                    onValueChange = { firstNameText = it },
+                    placeholder = { Text(text = "First Name") },
+                    textStyle = MaterialTheme.typography.body1
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+
+                OutlinedTextField(
+                    modifier = Modifier.height(56.dp),
+                    value = lastNameText,
+                    onValueChange = { lastNameText = it },
+                    placeholder = { Text(text = "Last Name") },
+                    textStyle = MaterialTheme.typography.body1
+                )
+
+            }
+
+            Spacer(modifier = Modifier.padding(8.dp))
 
             OutlinedTextField(
                 modifier = Modifier
                     .height(56.dp)
-                    .fillMaxWidth(0.5f),
-                value = firstNameText,
-                onValueChange = { firstNameText = it },
-                placeholder = { Text(text = "First Name") },
+                    .fillMaxWidth(),
+                value = emailAddressText,
+                onValueChange = { emailAddressText = it },
+                placeholder = { Text(text = "Email Address") },
                 textStyle = MaterialTheme.typography.body1
             )
 
             Spacer(modifier = Modifier.padding(8.dp))
 
             OutlinedTextField(
-                modifier = Modifier.height(56.dp),
-                value = lastNameText,
-                onValueChange = { lastNameText = it },
-                placeholder = { Text(text = "Last Name") },
-                textStyle = MaterialTheme.typography.body1
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth(),
+                value = passwordText,
+                onValueChange = { passwordText = it },
+                placeholder = { Text(text = "Password") },
+                textStyle = MaterialTheme.typography.body1,
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    Icon(
+                        modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible },
+                        imageVector = if (isPasswordVisible) Icons.Filled.Visibility
+                        else Icons.Filled.VisibilityOff,
+                        contentDescription = ""
+                    )
+                }
             )
 
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        OutlinedTextField(
-            modifier = Modifier
-                .height(56.dp)
-                .fillMaxWidth(),
-            value = emailAddressText,
-            onValueChange = { emailAddressText = it },
-            placeholder = { Text(text = "Email Address") },
-            textStyle = MaterialTheme.typography.body1
-        )
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        OutlinedTextField(
-            modifier = Modifier
-                .height(56.dp)
-                .fillMaxWidth(),
-            value = passwordText,
-            onValueChange = { passwordText = it },
-            placeholder = { Text(text = "Password") },
-            textStyle = MaterialTheme.typography.body1,
-            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            trailingIcon = {
-                Icon(
-                    modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible },
-                    imageVector = if (isPasswordVisible) Icons.Filled.Visibility
-                    else Icons.Filled.VisibilityOff,
-                    contentDescription = ""
+            if (isErrorMessageVisible) {
+                Text(
+                    text = errorMessage,
+                    color = MaterialTheme.colors.error
                 )
             }
-        )
 
-        if(isErrorMessageVisible){
             Text(
-                text = errorMessage,
-                color = MaterialTheme.colors.error
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .paddingFromBaseline(top = 24.dp),
+                text = termsAndConditionText,
+                style = MaterialTheme.typography.body2
             )
+
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            Button(
+                modifier = Modifier
+                    .height(48.dp)
+                    .fillMaxWidth(),
+                onClick = {
+                    isLoading = true
+                    viewmodel.createNewAccount(
+                        "$firstNameText $lastNameText",
+                        emailAddressText,
+                        passwordText
+                    )
+                },
+                shape = MaterialTheme.shapes.medium,
+                content = { Text(text = "Sign Up", fontWeight = FontWeight.Bold) }
+            )
+
         }
-
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .paddingFromBaseline(top = 24.dp),
-            text = termsAndConditionText,
-            style = MaterialTheme.typography.body2
-        )
-
-        Spacer(modifier = Modifier.padding(16.dp))
-
-        Button(
-            modifier = Modifier
-                .height(48.dp)
-                .fillMaxWidth(),
-            onClick = {
-                viewmodel.createNewAccount("$firstNameText $lastNameText",emailAddressText,passwordText)
-            },
-            shape = MaterialTheme.shapes.medium,
-            content = { Text(text = "Sign Up", fontWeight = FontWeight.Bold) }
-        )
-
     }
 }
