f7644a4d4e43d6b03b5c89ceb4715dd7eacf70cf
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-12 13:35:53 +0530

    docs: add kdoc to AuthenticationServiceException class

--- app/src/main/java/com/example/pocket/auth/AuthenticationServiceExceptions.kt
+++ app/src/main/java/com/example/pocket/auth/AuthenticationServiceExceptions.kt
@@ -1,5 +1,17 @@
 package com.example.pocket.auth
 
+/**
+ * The sub classes of this sealed class will be used by [AuthenticationResult.Failure].If a new kind of exception,
+ * related to authentication is to be created, then that exception will have to extend this sealed class in order to
+ * be used in conjunction with [AuthenticationResult.Failure].
+ *
+ * The [AuthenticationResult] class is the model class that is used for all authentication purposes in the entire app.
+ * This allows to easily switch out the [AuthenticationService] without breaking other code that depends up the exceptions
+ * thrown by the different implementations of [AuthenticationService].
+ *
+ * It must also be ensured that any implementation of [AuthenticationService] that makes use of [AuthenticationResult] must only throw
+ * exceptions that are subclasses of this class.
+ */
 sealed class AuthenticationServiceException(
     message: String? = null,
     cause: Throwable? = null
