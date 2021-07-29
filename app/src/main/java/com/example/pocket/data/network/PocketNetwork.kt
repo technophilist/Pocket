4e876eb8fd6c3d25c23153478dda768cb9010f13
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-07-29 22:41:49 +0530

    test(PocketNetwork): wrap all Connection.getDocument() inside runCatching

--- app/src/main/java/com/example/pocket/data/network/PocketNetwork.kt
+++ app/src/main/java/com/example/pocket/data/network/PocketNetwork.kt
@@ -9,8 +9,8 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
+import timber.log.Timber
 import java.net.URL
-import java.util.*
 
 
 interface Network {
@@ -62,53 +62,53 @@ class PocketNetwork(
      * @return the url of the image
      */
     private suspend fun getImageUrl(url: URL): String? = withContext(mDefaultDispatcher) {
-        val document = Jsoup.connect(url.toString()).getDocument()
+        runCatching {
+            val document = Jsoup.connect(url.toString()).getDocument()
 
-        //selecting all the meta elements
-        val metaElements = document.select("meta")
+            //selecting all the meta elements
+            val metaElements = document.select("meta")
 
-        //selecting all meta graph tags
-        val openGraphElements = metaElements.filter { it.attr("property").contains("og:") }
+            //selecting all meta graph tags
+            val openGraphElements = metaElements.filter { it.attr("property").contains("og:") }
 
-        //selecting the 'og:image' tag
-        val ogImageTag = openGraphElements.find { it.attr("property") == "og:image" }
+            //selecting the 'og:image' tag
+            val ogImageTag = openGraphElements.find { it.attr("property") == "og:image" }
 
-        //returning the value of the 'content' property which contains the url of the image
-        ogImageTag?.attr("content")
+            //returning the value of the 'content' property which contains the url of the image
+            ogImageTag?.attr("content")
+        }.getOrNull()
     }
 
     /**
      * Tries to fetch the favicon of a web page as a Drawable using the [url].
      * If the favicon is not found or glide throws an error , it will return null.
      */
-    override suspend fun fetchFavicon(url: URL): Drawable? =
-        withContext(mDefaultDispatcher) {
-            runCatching {
-                //try getting the image using /favicon.ico convention used in the web
+    override suspend fun fetchFavicon(url: URL): Drawable? = withContext(mDefaultDispatcher) {
+        runCatching {
+            //try getting the image using /favicon.ico convention used in the web
+            Glide.with(mContext)
+                .asDrawable()
+                .load("${url.protocol}://${url.host}/favicon.ico")
+                .getDownloadedResource()
+        }.getOrElse {
+            /*
+             * If it throws an error,try getting the favicon from the tags.
+             * If it is still not possible to get the favicon using the tags,
+             * return null.
+             */
+            getFaviconUrlFromTags(url)?.let { urlString ->
+                /*
+                we are not using a try/catch block because the
+                [getFaviconUrlFromTags] returns a url string only if
+                it can find a valid url to the favicon
+                 */
                 Glide.with(mContext)
                     .asDrawable()
-                    .load("${url.protocol}://${url.host}/favicon.ico")
+                    .load(urlString)
                     .getDownloadedResource()
-            }.getOrElse {
-
-                /*
-                 * If it throws an error,try getting the favicon from the tags.
-                 * If it is still not possible to get the favicon using the tags,
-                 * return null.
-                 */
-                getFaviconUrlFromTags(url)?.let { urlString ->
-                    /*
-                    we are not using a try/catch block because the
-                    [getFaviconUrlFromTags] returns a url string only if
-                    it can find a valid url to the favicon
-                     */
-                    Glide.with(mContext)
-                        .asDrawable()
-                        .load(urlString)
-                        .getDownloadedResource()
-                }
             }
         }
+    }
 
     /**
      * Tries to get the favicon of the website using the "shortcut icon" or "icon"
@@ -116,8 +116,8 @@ class PocketNetwork(
      * is not possible to find a link to the favicon, it returns null else it returns
      * the string representing the url of the favicon.
      */
-    private suspend fun getFaviconUrlFromTags(url: URL): String? {
-        return withContext(mDefaultDispatcher) {
+    private suspend fun getFaviconUrlFromTags(url: URL): String? = withContext(mDefaultDispatcher) {
+        runCatching {
             val document = Jsoup
                 .connect(url.toString())
                 .getDocument()
@@ -129,18 +129,11 @@ class PocketNetwork(
             val shortcutElements = linkElements.filter {
                 it.attr("rel") == "shortcut icon" || it.attr("rel") == "icon"
             }
-
-            try {
-                //selecting the first element and getting the url of the favicon
-                shortcutElements.first().attr("href")
-
-            } catch (exception: NoSuchElementException) {
-
-                //if it throws a NoSuchElementException , it means that the list is empty
-                //returning null since the list is empty
-                null
-            }
-        }
+            /* Select the first element and get the url of the favicon.
+             * If it throws a NoSuchElementException , it means that the list is empty.
+             */
+            shortcutElements.first().attr("href")
+        }.getOrNull()
     }
-
 }
+
