6a84de11842c8f2e390041f29a3c714fc9cd2af2
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-25 15:44:41 +0530

    refactor(MainActivityViewModelImpl): annot constructor with @Inject

--- app/src/main/java/com/example/pocket/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/pocket/viewmodels/MainActivityViewModel.kt
@@ -4,18 +4,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.pocket.data.Repository
 import com.example.pocket.data.preferences.PocketPreferences
+import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
+import javax.inject.Inject
 
 interface MainActivityViewModel {
     val currentAppTheme: LiveData<PocketPreferences.AppTheme>
     fun changeAppTheme(newTheme: PocketPreferences.AppTheme)
 }
 
-class MainActivityViewModelImpl(
+@HiltViewModel
+class MainActivityViewModelImpl @Inject constructor(
     private val mRepository: Repository
 ) : ViewModel(), MainActivityViewModel {
 
     override val currentAppTheme = mRepository.appTheme
     override fun changeAppTheme(newTheme: PocketPreferences.AppTheme) {
         mRepository.updateThemePreference(newTheme)
+    }
+
+    override fun onCleared() {
+        Timber.d("Cleard")
+        super.onCleared()
     }
 }
