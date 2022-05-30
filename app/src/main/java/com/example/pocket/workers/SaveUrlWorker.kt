6f180156e0c40caec54d387c4d3ad965c9e742ee
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-30 14:00:24 +0530

    refactor(SaveUrlWorker)!: add/inject defaultDispatcher in constructor

--- app/src/main/java/com/example/pocket/workers/SaveUrlWorker.kt
+++ app/src/main/java/com/example/pocket/workers/SaveUrlWorker.kt
@@ -5,11 +5,13 @@ import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.pocket.data.Repository
+import com.example.pocket.di.IoCoroutineDispatcher
 import com.example.pocket.di.PocketApplication
 import com.example.pocket.ui.activities.HandleUrlActivity
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import java.net.URL
 import javax.inject.Inject
 
@@ -21,12 +23,15 @@ import javax.inject.Inject
 class SaveUrlWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted workerParameters: WorkerParameters,
+    @IoCoroutineDispatcher private val defaultDispatcher: CoroutineDispatcher,
     private val repository: Repository
 ) : CoroutineWorker(context, workerParameters) {
-    override suspend fun doWork() =
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun doWork() = withContext(defaultDispatcher) {
         inputData.getString(HandleUrlActivity.EXTRA_URL)?.let {
             repository.saveUrl(URL(it))
             Result.success()
         } ?: Result.failure()
+    }
 }
 
