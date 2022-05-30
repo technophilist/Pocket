286fec8447843beb957cc0feec7944bbc401b498
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-30 13:37:12 +0530

    refactor(SaveUrlWorker)!: use hilt annot and add repository param

--- app/src/main/java/com/example/pocket/workers/SaveUrlWorker.kt
+++ app/src/main/java/com/example/pocket/workers/SaveUrlWorker.kt
@@ -1,24 +1,30 @@
 package com.example.pocket.workers
 
 import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.example.pocket.data.network.PocketNetwork
+import com.example.pocket.data.Repository
 import com.example.pocket.di.PocketApplication
 import com.example.pocket.ui.activities.HandleUrlActivity
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CoroutineDispatcher
 import java.net.URL
+import javax.inject.Inject
 
 /**
  * This worker is triggered whenever the user saves the url
  * from another app.
  */
-class SaveUrlWorker(
-    context: Context,
-    workerParameters: WorkerParameters
+@HiltWorker
+class SaveUrlWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
+    private val repository: Repository
 ) : CoroutineWorker(context, workerParameters) {
     override suspend fun doWork() =
         inputData.getString(HandleUrlActivity.EXTRA_URL)?.let {
-            val repository = (applicationContext as PocketApplication).appContainer.pocketRepository
             repository.saveUrl(URL(it))
             Result.success()
         } ?: Result.failure()
