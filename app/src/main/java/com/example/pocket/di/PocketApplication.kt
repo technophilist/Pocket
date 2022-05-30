951a2a7428f6e69341917a1cd4b825759c17e2f3
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-30 13:38:14 +0530

    refactor: replace default WorkManager initializer with Hilt generated intializer

--- app/src/main/java/com/example/pocket/di/PocketApplication.kt
+++ app/src/main/java/com/example/pocket/di/PocketApplication.kt
@@ -1,19 +1,26 @@
 package com.example.pocket.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.WorkerFactory
 import com.example.pocket.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class PocketApplication : Application() {
+class PocketApplication : Application(), Configuration.Provider {
 
-    lateinit var appContainer: AppContainer
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
-        appContainer = AppContainer(this)
     }
 
+    override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
 }
