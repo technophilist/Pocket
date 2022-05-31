303cd9d0c3be81e2924cbce1fb0e4cd7f9248d47
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-31 17:08:45 +0530

    refactor(HandleUrlActivity): set network constraints for worker

--- app/src/main/java/com/example/pocket/ui/activities/HandleUrlActivity.kt
+++ app/src/main/java/com/example/pocket/ui/activities/HandleUrlActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.pocket.R
 import com.example.pocket.auth.AuthenticationService
 import com.example.pocket.di.PocketApplication
@@ -41,25 +39,21 @@ class HandleUrlActivity : AppCompatActivity() {
             return
         }
         if (intent.type == "text/plain") {
-
-            //Building the data using data builder
             val dataBuilder = Data.Builder()
             dataBuilder.putString(EXTRA_URL, intent.getStringExtra(Intent.EXTRA_TEXT))
-
-            //Scheduling the work manager to save the url
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
             val workRequest = OneTimeWorkRequestBuilder<SaveUrlWorker>()
                 .setInputData(dataBuilder.build())
+                .setConstraints(constraints)
                 .build()
             WorkManager.getInstance(this).enqueue(workRequest)
-
-            //displaying toast
             Toast.makeText(
                 this,
                 resources.getString(R.string.label_added_to_pocket),
                 Toast.LENGTH_SHORT
             ).show()
-
-            //Closing the activity
             finish()
         }
 
