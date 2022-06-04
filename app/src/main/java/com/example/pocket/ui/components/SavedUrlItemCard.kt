dbdef6bfe1f3ba04fdb99bb8e4e0a7ba804ad269
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-06-04 15:55:55 +0530

    refactor: add SavedUrlItemCard composable

---
+++ app/src/main/java/com/example/pocket/ui/components/SavedUrlItemCard.kt
@@ -0,0 +1,108 @@
+package com.example.pocket.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.pocket.data.domain.SavedUrlItem
+
+//TODO add docs
+@Composable
+fun SavedUrlItemCard(
+    modifier: Modifier = Modifier,
+    savedUrlItem: SavedUrlItem,
+    thumbnail: ImageBitmap? = null,
+    favicon: ImageBitmap? = null
+) {
+    Card(modifier = modifier) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            if (savedUrlItem.imageAbsolutePath == null) {
+                //if the image path is null,display the host with primary background
+                Box(
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.primary)
+                        .fillMaxWidth()
+                        .weight(3f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = savedUrlItem.url.host,
+                        style = MaterialTheme.typography.h3,
+                        color = MaterialTheme.colors.onPrimary,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+
+            } else {
+                thumbnail?.let { imageBitmap ->
+                    Image(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(3f),
+                        bitmap = imageBitmap,
+                        contentDescription = "Thumbnail",
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+            Text(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(0.5f),
+                text = savedUrlItem.title,
+                style = MaterialTheme.typography.h1,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            UrlCardFooter(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.5f),
+                hostName = savedUrlItem.url.host,
+                favicon = favicon
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun UrlCardFooter(
+    modifier: Modifier = Modifier,
+    favicon: ImageBitmap? = null,
+    hostName: String
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        favicon?.let {
+            Image(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(24.dp)
+                    .clip(CircleShape),
+                bitmap = it,
+                contentDescription = "Thumbnail",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(
+            modifier = Modifier.padding(start = 8.dp),
+            text = hostName,
+            style = MaterialTheme.typography.caption
+        )
+    }
+}
