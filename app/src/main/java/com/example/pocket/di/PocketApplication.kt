1b6afbea3387a2d6dea69ffcdf498c77f2dc3942
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-06-10 15:41:38 +0530

    refactor(PocketApplication): enqueue CleanupDeletedItemsWorker in onCreate()

--- app/src/main/java/com/example/pocket/di/PocketApplication.kt
+++ app/src/main/java/com/example/pocket/di/PocketApplication.kt
@@ -2,11 +2,12 @@ package com.example.pocket.di
 
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import androidx.work.WorkerFactory
+import androidx.work.*
 import com.example.pocket.BuildConfig
+import com.example.pocket.workers.CleanUpDeletedItemsWorker
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import java.time.Duration
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -17,10 +18,31 @@ class PocketApplication : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
+        enqueueCleanUpWorker()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun enqueueCleanUpWorker() {
+        val repeatInterval = Duration.ofHours(24)
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val workRequest = PeriodicWorkRequestBuilder<CleanUpDeletedItemsWorker>(repeatInterval)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            CLEANUP_DELETED_ITEMS_WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+        )
     }
 
     override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
         .setWorkerFactory(workerFactory)
         .build()
+
+    companion object {
+        const val CLEANUP_DELETED_ITEMS_WORK_NAME =
+            "com.example.pocket.di.PocketApplication.CLEANUP_DELETED_ITEMS_WORK_NAME"
+    }
 }
