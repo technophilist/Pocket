039fd4d071d6ad26c589de545b0b4318d32f7f6f
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-06-10 16:17:17 +0530

    refactor: add junit 4 dependency

---
+++ app/src/androidTest/java/com/example/pocket/workers/CleanUpDeletedItemsWorkerTest.kt
@@ -0,0 +1,17 @@
+package com.example.pocket.workers
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class CleanUpDeletedItemsWorkerTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
